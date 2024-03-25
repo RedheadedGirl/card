46121bd4fdad41449a2d4981faa9dd04701f4e98
RedheadedGirl <megandoe33@gmail.com>
2024-03-25 22:51:09 +0300

    chore: добавлена валидация в ClientInput

--- src/main/java/ru/sbrf/dto/ClientInput.java
+++ src/main/java/ru/sbrf/dto/ClientInput.java
@@ -1,14 +1,23 @@
 package ru.sbrf.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 import java.time.LocalDate;
 
 @Value
-public class ClientInput { // расставить валидацию
-    private short id;
+public class ClientInput {
+
+    @NotBlank
     private String fio;
+
+    @NotNull
     private LocalDate birthday;
+
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String inn;
 }
