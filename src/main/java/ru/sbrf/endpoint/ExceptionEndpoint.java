61cf1f10c6799df1187ae57aef72e246b985c2fd
RedheadedGirl <megandoe33@gmail.com>
2024-03-25 22:44:42 +0300

    deps: добавлен log4j2

--- src/main/java/ru/sbrf/endpoint/ExceptionEndpoint.java
+++ src/main/java/ru/sbrf/endpoint/ExceptionEndpoint.java
@@ -1,16 +1,14 @@
 package ru.sbrf.endpoint;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.sbrf.exceptions.ErrorInfo;
 import ru.sbrf.exceptions.ServiceException;
 
+@Log4j2
 @RestControllerAdvice
 public class ExceptionEndpoint extends ResponseEntityExceptionHandler {
 
@@ -23,7 +21,7 @@ public class ExceptionEndpoint extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<ErrorInfo> onServiceException(ServiceException ex) {
-//        log.error(ex.getMessage(), ex);
+        log.error(ex.getMessage(), ex);
         return ResponseEntity.status(ex.getHttpStatus()).body(ex.getErrorInfo());
     }
 }
